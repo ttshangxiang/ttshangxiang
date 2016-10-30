2eee06cb870074411cd79f5febbafcbb30de307e
ttshangxiang <ttshangxiang@qq.com>
2016-10-31 00:58:44 +0800

    card

---
+++ client/src/components/Card.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+require('styles/Card.css');
+
+class AppComponent extends React.Component {
+    render() {
+        let data = this.props.data;
+        return (
+            <div className="card">
+                <div className="content">
+                    <div className="warp">
+                        <div className="img">
+                            <a href="javascript:;">
+                                <span className="f-trans piccover">&nbsp;</span>
+                                <img className="f-trans" src={data.img} alt="" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="text">
+                        <p>{data.text}</p>
+                    </div>
+                </div>
+                <div className="f-clear otherinfo">
+                    <a href="#" className="time">{data.time}</a>
+                    <a href="#" className="f-icon hot">{data.zan}</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,18 +2,33 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import Card from './Card';
 
-let yeomanImage = require('../images/yeoman.png');
+let img = require('../images/1293377518002363058.jpg');
+let data = {
+    img: img,
+    text: '这张是前几天，周6，在回家路上拍的。',
+    time: '2011-08-24',
+    zan: 796
+}
+
+let img2 = require('../images/2891873910742182051.jpg');
+let data2 = {
+    img: img2,
+    text: '专注，从LOFTER开始',
+    time: '2011-08-25',
+    zan: 359
+}
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/component/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="index">
+                <Card data={data}></Card>
+                <Card data={data2}></Card>
+            </div>
+        );
+    }
 }
 
 AppComponent.defaultProps = {

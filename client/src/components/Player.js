087f6e96b4b8c2fd6bee5f25e79c2aa581bf2298
ttshangxiang <ttshangxiang@gmail.com>
2016-11-10 18:55:55 +0800

    播放器

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -5,8 +5,8 @@ import React from 'react';
 class Player extends React.Component {
 
     render() {
-        const cuseqi = require('../images/words/1293377518002363058.jpg');
-        // const head = require('../images/yeoman.png');
+        const bg_img = require('../images/music/niaoshouxihua.jpg');
+        const head = require('../images/music/niaoshouxihua.jpg');
         return (
             <div className="s_player">
                 <div className="btn-box">
@@ -14,9 +14,16 @@ class Player extends React.Component {
                     <div className="like btn"></div>
                 </div>
                 <div className="info">
-                    <img className="bg" src={cuseqi} alt=""/>
+                    <img className="bg" src={bg_img} alt=""/>
                     <div className="content">
-                        <img className="head" src={cuseqi} alt=""/>
+                        <img className="head" src={head} alt=""/>
+                        <div className="song_title">鳥獣戯画</div>
+                        <div className="song_artist">梶迫小道具店</div>
+                    </div>
+                    <div className="song-btn">
+                        <div className="loop"></div>
+                        <div className="voice"></div>
+                        <div className="voice-num"></div>
                     </div>
                 </div>
                 <div className="btn-box">
